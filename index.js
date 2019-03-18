8456f24f5865f68cc58f82b5a3abdf7c761b38be
jianl <jianjianfengyun@126.com>
2019-03-18 17:29:52 +0800

    fix entry

---
+++ index.js
@@ -0,0 +1,44 @@
+const _ = require('lodash');
+
+
+const getEntry = (entry) => {
+  if (typeof entry === 'function') {
+    return () => Promise.resolve(entry()).then(getEntry);
+  }
+
+  if (typeof entry === 'object' && !Array.isArray(entry)) {
+    return () =>  Promise.resolve(entry)
+  }
+
+  return () => Promise.resolve(entry);
+};
+
+const injectText = (source) => {
+  if (/\bmodule.hot\b/.test(source)) {
+    return source;
+  }
+  return `
+${source}
+if(module.hot){
+  module.hot.accept()
+}
+`
+};
+
+module.exports = function(source, map, meta) {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  this.async();
+
+  const rawRequest = this._module.rawRequest;
+
+  getEntry(this._compiler.options.entry)().then((entry)=>{
+    if(_.includes(entry, rawRequest)){
+      source = injectText(source);
+    }
+    this.callback(null, source);
+  })
+
+};
